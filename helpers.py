7186b3516db58e7115d34de8eb8d4e4e29fbcb5b
tanya1028 <46280518+tanya1028@users.noreply.github.com>
2021-07-08 13:28:04 +0530

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,13 @@
+t_input = """
+MDTextFieldRect:
+hint_text: "Type here..."
+pos_hint:{'center_x': 0.5, 'center_y': .6}
+size_hint_y:.6
+size_hint_x:.8
+ """
+t_output = """
+MDTextFieldRect:
+pos_hint:{'center_x': 0.5, 'center_y': 0.}
+size_hint_y:.6
+size_hint_x:.8
+"""
